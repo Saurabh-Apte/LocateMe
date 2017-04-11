c522fc7f3b706580b5443bf9868fe03acfd27e2d
masterapte <saurabhapte105@gmail.com>
2017-04-11 07:19:46 +0530

    Add files via upload

---
+++ config.dist.js
@@ -0,0 +1,7 @@
+var config = {
+  firebase: 'https://locatme-5d18e.firebaseio.com/',
+  mapbox: {
+      accessToken: 'pk.eyJ1IjoibWFzdGVyYXB0ZSIsImEiOiJjajExaGZwNGwwMDF3MnhyeGk2azQ3Y2JxIn0.tLa3GGHHJRupgG_bm2z9hw',
+      mapId: 'mapbox.satellite',
+  }
+}
