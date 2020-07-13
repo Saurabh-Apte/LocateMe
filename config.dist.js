5bd22cf74f57982a3e2de918281856362f8e93dd
Saurabh Apte <saurabhapte105@gmail.com>
2020-07-13 00:45:15 -0400

    Update config.dist.js

--- config.dist.js
+++ config.dist.js
@@ -1,5 +1,5 @@
 var config = {
-  firebase: 'https://locatme-5d18e.firebaseio.com/',
+  firebase: 'locatme-5d18e',
   mapbox: {
       accessToken: 'pk.eyJ1IjoibWFzdGVyYXB0ZSIsImEiOiJjajExaGZwNGwwMDF3MnhyeGk2azQ3Y2JxIn0.tLa3GGHHJRupgG_bm2z9hw',
       mapId: 'mapbox.satellite',
